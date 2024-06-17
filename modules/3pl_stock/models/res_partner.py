3cacceb9fc9e9bce1d67c05f41e5bf70c7709aaa
Igor Horun <igor.goroun@gmail.com>
2024-06-17 17:22:07 +0300

    Add auth update function

--- modules/3pl_stock/models/res_partner.py
+++ modules/3pl_stock/models/res_partner.py
@@ -1,4 +1,7 @@
+import json
 import logging
+import bcrypt
+from passlib.context import CryptContext
 from odoo import models, fields, api
 
 
@@ -12,3 +15,21 @@ class ResPartner(models.Model):
     api_login = fields.Char(string='API Login', required=False, )
     api_key = fields.Char(string='API Key', required=False, )
 
+    def list_auth_data(self):
+        auth_data = {}
+        active_api_clients = self.search([
+            ('active', '=', True),
+            ('api_connected', '=', True),
+            ('api_login', '!=', False),
+            ('api_key', '!=', False)])
+        if active_api_clients.exists():
+            pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+            for partner in active_api_clients:
+                auth_data.update({
+                    partner.api_login: {
+                        'api_login': partner.api_login,
+                        'api_key': pwd_context.hash(partner.api_key),
+                        'partner_id': partner.id,
+                    }
+                })
+        return auth_data
